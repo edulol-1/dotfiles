ecebc175afdca29aa5bfb088f7e1f39d3a188876
acyclic <someemail@gmail.com>
2026-06-17 00:14:09 -0600

    Add dotfiles for emacs and vim

---
+++ .vimrc
@@ -0,0 +1,11 @@
+filetype plugin indent on
+
+set tabstop=4
+set shiftwidth=4
+set expandtab
+"set autoindent
+
+" For searching
+set incsearch
+set nohlsearch
+set ignorecase
---
+++ init.el
@@ -0,0 +1,25 @@
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+;; Comment/uncomment this line to enable MELPA Stable if desired.  See `package-archive-priorities`
+;; and `package-pinned-packages`. Most users will not need or want to do this.
+;;(add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/") t)
+(package-initialize)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("f1c8202c772d1de83eda4765fe21429a528a4fb350a28394d3705fe9678ed1f9" default))
+ '(package-selected-packages '(adwaita-dark-theme acme-theme counsel)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+(setq-default indent-tabs-mode nil)
+(setq-default tab-width 4)
+(setq-default c-basic-offset 4)
+(set-face-attribute 'default nil :height 105)
